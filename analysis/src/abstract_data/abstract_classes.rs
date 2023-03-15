1d4e45dc2789dc6d61e76cad98f17b46757f702d
sking8484 <sking8484@gmail.com>
2023-03-15 00:16:52 -0400

    Added diagonalize matrix code to analysis methods

--- analysis/src/abstract_data/abstract_classes.rs
+++ analysis/src/abstract_data/abstract_classes.rs
@@ -1,5 +1,9 @@
-use polars::prelude::*;
+use polars::{prelude::*, export::num::Zero};
+use ndarray::{Array1, Array2};
 
 pub trait AnalysisToolKit {
     fn calculate_num_rows(&self, data: &DataFrame) -> usize;
+    fn diagonalize_array<T>(&self, data: &Array1<T>) -> Array2<T>
+    where
+        T: Clone + Zero;
 }
--- analysis/src/lib.rs
+++ analysis/src/lib.rs
@@ -1,6 +1,7 @@
 use crate::abstract_data::abstract_classes::AnalysisToolKit;
 pub mod abstract_data;
-use polars::prelude::*;
+use polars::{prelude::*, export::num::Zero};
+use ndarray::{Array1, Array2};
 
 pub struct AnalysisMethods {}
 
@@ -8,12 +9,20 @@ impl AnalysisToolKit for AnalysisMethods {
     fn calculate_num_rows(&self, data: &DataFrame) -> usize {
         data.height()
     }
+    fn diagonalize_array<T>(&self, data: &Array1<T>) -> Array2<T> 
+    where 
+        T: Clone + Zero 
+    {
+        let diagonal_matrix: Array2<T> = Array2::from_diag(&data);
+        diagonal_matrix
+    }
 }
 
 
 
 #[cfg(test)]
 mod tests {
+    use ndarray::{arr1, arr2};
     use super::*;
 
     #[test]
@@ -22,5 +31,13 @@ mod tests {
         let frame: DataFrame = df!("data" => &["1", "2", "3"])
             .expect("We should see a df");
         assert_eq!(methods.calculate_num_rows(&frame), 3)
+    }
+
+    #[test]
+    fn diagonalize_array() {
+        let methods = AnalysisMethods{};
+        let array = arr1(&[1, 2]);
+        let diagonals = methods.diagonalize_array(&array);
+        assert_eq!(diagonals, arr2(&[[1, 0], [0, 2]]))
     }
 }
