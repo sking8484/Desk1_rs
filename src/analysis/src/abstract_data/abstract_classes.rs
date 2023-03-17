4a660ddefb12b14b6057c818c00b70457c4c2ba3
sking8484 <sking8484@gmail.com>
2023-03-17 01:29:13 -0400

    Terrible implementation of divide matrices.
    This implementation needs to be updated.

--- src/analysis/src/abstract_data/abstract_classes.rs
+++ src/analysis/src/abstract_data/abstract_classes.rs
@@ -1,12 +1,21 @@
-use polars::{prelude::*, export::num::{Zero, FromPrimitive, Float}}; use ndarray::{Array1, Array2, RemoveAxis, Array, OwnedRepr};
+use std::{ops::Div, fmt::Debug};
+
+use polars::{prelude::*, export::num::{Zero, FromPrimitive, Float}}; use ndarray::{Array1, Array2, RemoveAxis, Array, OwnedRepr, Dimension, DimMax, DataOwned, DataMut};
 
 pub trait AnalysisToolKit {
     fn calculate_num_rows(&self, data: &DataFrame) -> usize;
     fn diagonalize_array<T>(&self, data: &Array1<T>) -> Array2<T>
         where
             T: Clone + Zero;
-    fn calculate_std<T>(&self, data: &Array2<T>, axis: usize, ddof: T) -> Array1<T>
+    fn calculate_std<T, D>(&self, data: &Array<T, D>, axis: usize, ddof: T) -> Array<T, D::Smaller>
         where 
-            T: Clone + Zero + FromPrimitive + Float;
+            T: Clone + Zero + FromPrimitive + Float,
+            D: Dimension + RemoveAxis;
+
+    fn divide_matrices<T, D>(&self, lhs: &Array<T, D>, rhs: &Array<T, D>)
+        where
+            T: Clone + Zero + FromPrimitive + Float + Debug,
+            D: Dimension + RemoveAxis;
+            
 }
 
--- src/analysis/src/lib.rs
+++ src/analysis/src/lib.rs
@@ -1,7 +1,9 @@
+use std::{ops::Div, fmt::Debug};
+
 use crate::abstract_data::abstract_classes::AnalysisToolKit;
 pub mod abstract_data;
 use polars::{prelude::*, export::num::{Zero, FromPrimitive, Float}};
-use ndarray::{Array1, Array2, Axis, RemoveAxis, Array};
+use ndarray::{Array1, Array2, Axis, RemoveAxis, Array, Dimension, DataOwned, DataMut, DimMax};
 
 pub struct AnalysisMethods {}
 
@@ -16,13 +18,29 @@ impl AnalysisToolKit for AnalysisMethods {
         let diagonal_matrix: Array2<T> = Array2::from_diag(&data);
         diagonal_matrix
     }
-    fn calculate_std<T>(&self, data: &Array2<T>, axis: usize, ddof: T) -> Array1<T>
+    fn calculate_std<T, D>(&self, data: &Array<T, D>, axis: usize, ddof: T) -> Array<T, D::Smaller>
         where
-            T: Clone + Zero + FromPrimitive + Float {
+            T: Clone + Zero + FromPrimitive + Float,
+            D: Dimension + RemoveAxis {
         return data.std_axis(Axis(axis), ddof)
-        
     }
-}
+    fn divide_matrices<T, D>(&self, lhs: &Array<T, D>, rhs: &Array<T, D>)
+            where
+                T: Clone + Zero + FromPrimitive + Float + Debug,
+                D: Dimension + RemoveAxis {
+        let mut iter1 = lhs.indexed_iter();
+        let mut iter2 = rhs.indexed_iter();
+        let mut return_matrix = Array::zeros((3, 2));
+        let return_iter = return_matrix.iter_mut();
+        for val in return_iter {
+            let val1 = iter1.next().unwrap().1;
+            let val2 = *iter2.next().unwrap().1;
+            *val = *val1 / val2;
+        }
+        for val in return_matrix.into_iter() {
+            println!("{:?}", val)
+        }
+}   }
 
 
 
@@ -55,5 +73,16 @@ mod tests {
                [5., 6.]]);
         let stddev = methods.calculate_std(&a, 0, 1.);
         assert_eq!(stddev, aview1(&[2., 2.]));
+    }
+
+    #[test]
+    fn divide_matrices() {
+        let methods = AnalysisMethods{};
+        let a = arr2(&[
+                [1., 2.],
+                [3., 4.],
+                [5., 6.]
+        ]);
+        methods.divide_matrices(&a, &a)
     }
 }
