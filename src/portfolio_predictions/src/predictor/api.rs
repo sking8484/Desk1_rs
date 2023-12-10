c0f56423718d65f312ee8a54f0baca400eb3d915
sking8484 <sking8484@gmail.com>
2023-12-10 16:04:53 -0500

    Finishes mssa predictor

--- src/portfolio_predictions/src/predictor/api.rs
+++ src/portfolio_predictions/src/predictor/api.rs
@@ -2,6 +2,8 @@ extern crate polars;
 
 use polars::prelude::*;
 
+use super::mssa::{MssaError, MssaPredictor};
+
 #[derive(Debug)]
 pub enum PredictionError {
     TrainModelError,
@@ -10,7 +12,7 @@ pub enum PredictionError {
 }
 
 pub trait Predictor {
-    fn test(&self) -> bool; 
+    fn new(num_days_per_col: i64, total_trailing_days: i64, num_assets: i64, hsvt_threshold: f64) -> Result<MssaPredictor, MssaError>;
     fn retrieve_formatted_data(&self) -> Result<DataFrame, PolarsError>;
     fn build_prediction_data(&self, cleaned_data: DataFrame) -> Result<DataFrame, PolarsError>;
     fn create_predictions(&self, prediction_data: DataFrame) -> DataFrame;
